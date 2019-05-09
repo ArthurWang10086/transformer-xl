14613bbc6996e2552ec209b16920553423aa091d
kuanke <asdqsczser@gmail.com>
2019-05-09 11:32:53 +0800

    wt103

--- tf/sota/download.sh
+++ tf/sota/download.sh
@@ -18,40 +18,6 @@ function download () {
 cd $DATA_ROOT
 mkdir -p pretrained_xl && cd pretrained_xl
 
-# enwik8
-mkdir -p tf_enwik8 && cd tf_enwik8
-
-mkdir -p data && cd data
-download ${URL}/tf_enwiki8/data/cache.pkl
-download ${URL}/tf_enwiki8/data/corpus-info.json
-cd ..
-
-mkdir -p model && cd model
-download ${URL}/tf_enwiki8/model/checkpoint
-download ${URL}/tf_enwiki8/model/model.ckpt-0.data-00000-of-00001
-download ${URL}/tf_enwiki8/model/model.ckpt-0.index
-download ${URL}/tf_enwiki8/model/model.ckpt-0.meta
-cd ..
-
-cd ..
-
-# text8
-mkdir -p tf_text8 && cd tf_text8
-
-mkdir -p data && cd data
-download ${URL}/tf_text8/data/cache.pkl
-download ${URL}/tf_text8/data/corpus-info.json
-cd ..
-
-mkdir -p model && cd model
-download ${URL}/tf_text8/model/checkpoint
-download ${URL}/tf_text8/model/model.ckpt-0.data-00000-of-00001
-download ${URL}/tf_text8/model/model.ckpt-0.index
-download ${URL}/tf_text8/model/model.ckpt-0.meta
-cd ..
-
-cd ..
-
 # wt103
 mkdir -p tf_wt103 && cd tf_wt103
 
@@ -69,19 +35,4 @@ cd ..
 
 cd ..
 
-# lm1b
-mkdir -p tf_lm1b && cd tf_lm1b
 
-mkdir -p data && cd data
-download ${URL}/tf_lm1b/data/cache.pkl
-download ${URL}/tf_lm1b/data/corpus-info.json
-cd ..
-
-mkdir -p model && cd model
-download ${URL}/tf_lm1b/model/checkpoint
-download ${URL}/tf_lm1b/model/model.ckpt-1191000.data-00000-of-00001
-download ${URL}/tf_lm1b/model/model.ckpt-1191000.index
-download ${URL}/tf_lm1b/model/model.ckpt-1191000.meta
-cd ..
-
-cd ..
